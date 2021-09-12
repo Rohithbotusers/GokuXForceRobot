cdaa021b8d053f56faf2d981db4b22dd219e3af7
Rohithbotusers <90542170+Rohithbotusers@users.noreply.github.com>
2021-09-12 16:57:02 +0530

    Update __main__.py

--- XForceSTB/__main__.py
+++ XForceSTB/__main__.py
@@ -677,7 +677,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[I am now online!](https://telegra.ph/file/62be8da2c0a8e9b57b148.mp4)", parse_mode=ParseMode.MARKDOWN)
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[I am the world best group managemed bot!](https://telegra.ph/file/87affb170718b0dc07c4c.mp4)", parse_mode=ParseMode.MARKDOWN)
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!",
